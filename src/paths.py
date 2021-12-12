eaec58b5b1c54695d787cf4aafa7747df55d34b4
Sherman Siu <s8siu@uwaterloo.ca>
2021-12-12 17:13:07 -0500

    Update paths.

--- src/paths.py
+++ src/paths.py
@@ -5,6 +5,9 @@ BASE_PATH = pathlib.Path(__file__).parent.parent
 # Word list
 WORDLIST_ENG_PATH = BASE_PATH.joinpath("wordlist-eng.txt")
 
+# Word vectors
+GLOVE_PATH = BASE_PATH.parent.joinpath("codenames/dataset/glove.6B.300d.npy")
+
 # Allowed
 GOOGLE_10K_ENG_PATH = BASE_PATH.joinpath("google-10000-english-no-swears.txt")
 WIKI_100K_PATH = BASE_PATH.joinpath("wiki-100k.txt")
