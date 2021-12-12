9ce7f80d1c9de4b4d1919fd54bbaa176a779232a
Sherman Siu <s8siu@uwaterloo.ca>
2021-12-12 17:08:47 -0500

    Update paths.

--- src/paths.py
+++ src/paths.py
@@ -1,7 +1,14 @@
 import pathlib
 
 BASE_PATH = pathlib.Path(__file__).parent.parent
+
+# Word list
 WORDLIST_ENG_PATH = BASE_PATH.joinpath("wordlist-eng.txt")
+
+# Allowed
 GOOGLE_10K_ENG_PATH = BASE_PATH.joinpath("google-10000-english-no-swears.txt")
-DIRECTIONAL_PATH = BASE_PATH.joinpath("directional.txt")
+WIKI_100K_PATH = BASE_PATH.joinpath("wiki-100k.txt")
 CUSTOM_WHITELIST = BASE_PATH.joinpath("custom_whitelist.txt")
+
+# Illegal
+DIRECTIONAL_PATH = BASE_PATH.joinpath("directional.txt")
