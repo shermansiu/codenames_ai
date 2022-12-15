aa1582d30e7ff55c495a5b31c6cac693e417af73
Sherman Siu <s8siu@uwaterloo.ca>
2022-12-15 16:36:21 -0500

    Fix GloVe vector acquisition scripts.

--- embeddings/get_glove_CC_42B-300d.sh
+++ embeddings/get_glove_CC_42B-300d.sh
@@ -1,3 +1,3 @@
 wget -N --no-check-certificate https://nlp.stanford.edu/data/glove.42B.300d.zip
 unzip glove.42B.300d.zip glove.42B.300d.txt
-python convert.py glove.42B.300d
+python convert_glove.py glove.42B.300d
