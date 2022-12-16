927671d3677f56021fdae56e450433771586ed5b
Sherman Siu <s8siu@uwaterloo.ca>
2022-12-16 14:03:42 -0500

    Update whack-a-mole environment.

--- src/gridtest_env.py
+++ src/gridtest_env.py
@@ -141,22 +141,22 @@ class GridTestStackedEnv(GridTestEnv):
         return super().step(action)
 
 
-class WhackAMoleEnv(gym.Env):
+class WhackAMoleMultiBinaryEnv(gym.Env):
     """Codenames environment for gym."""
 
     environment_name = "Whack-a-mole v0.0.1"
     metadata = {"render.modes": ["human"]}
 
     def __init__(
-        self, length: int = 5, seed: tp.Optional[int] = None
+        self, length: int = 5, seed: tp.Optional[int] = None, max_steps: tp.Optional[int] = None
     ):
         super().__init__()
         self.length = length
         self.size = self.length*self.length
-        self.action_space = spaces.MultiDiscrete(self.size)
-        observation_shape = (self.length, self.length, 1)
+        self.action_space = spaces.MultiBinary(self.size)
+        observation_shape = (self.length, self.length, 3)
         self.observation_space = spaces.Box(
-            low=0, high=255, shape=observation_shape, dtype=np.uint8
+            low=-1, high=1, shape=observation_shape, dtype=np.int8
         )
         self.step_reward_if_win = 0
         self.step_reward_if_not_end = -1
@@ -184,45 +184,63 @@ class WhackAMoleEnv(gym.Env):
 
     def start_new_game(self):
         self.current_step = 0
-        self.correct = self.np_random.integers(self.size)
-        self.board = np.zeros(self.size, dtype=np.int32)
-        self.board[self.correct] = 1
+        self.prev_board = np.zeros(self.size, dtype=np.int8)
+        self.prev_board_altered = np.zeros(self.size, dtype=np.int8)
+        self.prev_action = np.zeros(self.size, dtype=np.int8)
+        self.board = self.np_random.integers(2, size=self.size, dtype=np.int8)
 
     def reset(self):
         # Reset the state of the environment to an initial state
         self.start_new_game()
         return self.current_observation()
 
+    def as_rgb_screen(self, board):
+        return board.reshape(-1, self.length)  # *128+128
+
     def current_observation(self):
-        return np.clip(self.board.reshape(-1, self.length, 1)*128+128, 0, 255).astype(np.uint8)
+        # stacked_boards = np.stack([
+        #     self.as_rgb_screen(self.prev_board),
+        #     self.as_rgb_screen(self.prev_board_altered),
+        #     self.as_rgb_screen(self.board)
+        # ], axis=-1)
+        stacked_board = self.as_rgb_screen(self.prev_board)[..., None]
+        return np.clip(stacked_boards, -1, 1).astype(np.int8)
 
     def is_done(self):
-        return self.current_step >= self.max_steps or bool(self.board[self.correct] == 0)
+        return self.current_step >= self.max_steps
 
-    def compute_reward(self):
-        if self.is_done():
-            return self.step_reward_if_win
-        return self.step_reward_if_not_end
+    def compute_reward(self, action):
+        return (action == self.board).sum() / self.board.size
 
     def step(self, action):
         self.current_step += 1
-        if not self.is_done():
-            self.board[action] = max(self.board[action], 0) - 1
+        done = self.is_done()
+        reward = 0.0
+        if not done:
+            reward = self.compute_reward(action)
+            self.prev_board = self.board
+            self.prev_action = action
+            self.prev_board_altered = self.board-action
+            self.board = self.np_random.integers(2, size=self.size, dtype=np.int8)
         return (
             self.current_observation(),
-            self.compute_reward(),
-            self.is_done(),
+            reward,
+            done,
             self.debug_info(),
         )
 
     def render(self, mode="human", close=False):
         # Render the environment to the screen
-        print("Correct:", self.correct)
-        print("Board:", self.current_observation())
+        print("Prev board:", self.prev_board)
+        print("Prev action:", self.prev_action)
+        print("Prev board (altered):", self.prev_board_altered)
+        print("Board:", self.board)
         print()
 
     def debug_info(self):
         return dict(
-            correct=self.correct,
+            prev_board=self.prev_board,
+            prev_action=self.prev_action,
+            prev_board_altered=self.prev_board_altered,
             board=self.board
         )
